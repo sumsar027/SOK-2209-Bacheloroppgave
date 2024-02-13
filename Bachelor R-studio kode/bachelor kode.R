82027b66d131fd04765b18caf4c814c2b2f79593
sumsar027 <93335201+sumsar027@users.noreply.github.com>
2024-02-13 21:22:43 +0100

    Update bachelor kode.R

--- Bachelor R-studio kode/bachelor kode.R
+++ Bachelor R-studio kode/bachelor kode.R
@@ -17,6 +17,7 @@ setwd("C:\\Users\\Eier\\Downloads\\Data for bacheloroppgave")
   library(scales)
   library(readr)
   library(PxWebApiData)
+  library(naniar) # For å sette NA verdier, https://cran.r-project.org/web/packages/naniar/vignettes/replace-with-na.html
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Henting av datasett "Our World in Data, Trust in Europa" og "Our World in Data, Trust in Government"
@@ -54,7 +55,8 @@ df <- df %>% rename(sivilstatus = r26_bgciv, type_området = r26k2_bgurb, høyes
                                 vurdering_livet = r26_pcsal, investering_utdanning = r26k2_powpr_2, 
                                   valg_parti = r26k2_pcpar, vurdering_demokrati = r26k2_pcsad, 
                                     trygghet_nær = r26_pcsaf, mening_innvandrere_bo = r26_pcimm, 
-                                      mening_innvandrere = r26_pcldi,
+                                      mening_innvandrere = r26_pcldi, jobb_tilstand = r26_bgday, 
+                                        statsborgerskap = w01_k28, 
                     
                     innvandring1 = w01_k29, innvandring2 = w03_r3k29, innvandring3 = r4k29, innvandring4 = r5k29,
                     innvandring5 = r11bk29, innvandring6 = r14bk29, innvandring7 = r16bk29, innvandring8 = r18bk29,
@@ -67,65 +69,113 @@ df <- subset(df, !(tillit_stortinget %in% c(97, 98)))
 
 # Lager nytt datasett med de variablene vi skal bruke for en enklere visualisering 
 
-valgt_df = select(df, kjønn, fødselsår, sivilstatus, innvandring1, landsdel, fylke, høyest_utdanning,
-                    brutto_inntekt, type_arbeid, vurdering_egenøkonomi, tillit, politisk_tillit, 
-                      tillit_stortinget, tillit_media, vurdering_regjering, interesse_politikk, 
-                        valg_parti, vurdering_demokrati, vurdering_livet, investering_utdanning, 
-                          trygghet_nær, mening_innvandrere_bo, mening_innvandrere)
+valgt_df = select(df, kjønn, fødselsår, sivilstatus, statsborgerskap, innvandring1, landsdel, fylke, 
+                    høyest_utdanning, brutto_inntekt, type_arbeid, jobb_tilstand, vurdering_egenøkonomi, 
+                      tillit, politisk_tillit, tillit_stortinget, tillit_media, vurdering_regjering, 
+                        interesse_politikk, valg_parti, vurdering_demokrati, vurdering_livet, 
+                          investering_utdanning, trygghet_nær, mening_innvandrere_bo, mening_innvandrere)
 
 
-
-# Lager en visuell beskrivelse av utdanning ved bruk av barplot 
-
-par(mar=c(6,3,1,1))
-
-utdanning1 <- 
-  barplot(table(valgt_df$høyest_utdanning), 
-          names.arg=c("Ingen/Grunnskole","VGS","Universitet/høgskole","Ikke svart"),
-          las=2, 
-          main="Høyeste utdanningsnivå",
-          ylab="Antall",
-          border="lightblue",
-          col=valgt_df$høyest_utdanning)
-
-# Lager en visuell beskrivelse av fødselsår
-
-fødselsår1 <- 
-  barplot(table(valgt_df$fødselsår),
-          names.arg=c("1939_og_før", "1940-1949", "1950-1959", "1960-1969", "1970-1979", "1980-1989", "1990_og_etter"),
-                      las = 2,
-                      main = "Fødselsår",
-                      ylab = "Antall",
-                      border = "lightblue",
-                      col = valgt_df$fødselsår)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Emperi for å finne referansegruppene 
 
+# Endrer alle 97 og 98 verdier til NAs
+
+empiri <- valgt_df %>% replace_with_na_all(condition = ~.x == 97)
+empiri <- empiri %>% replace_with_na_all(condition = ~.x == 98)
+
+# Ser gjennom en oppsummering av alle variablene
+summary(empiri)
+
+# Går gjennom antall observasjoner for de forskjellige svaralternativene 
+# for hver variabel i datasettet
+
+antall_observasjoner <- function(valgt_df) {
+  for (col in names(valgt_df)) {
+    cat("Variabel:", col, "\n")
+    cat("Antall svaralternativer:", length(unique(valgt_df[[col]])), "\n")
+    cat("Antall observasjoner:\n")
+    print(table(valgt_df[[col]]))
+    cat("\n")
+  }}
+
+antall_observasjoner(valgt_df)
 
 # Gjennomgang av variabler, referansegruppe;
 
-# Utdanning: Videregående eller høyere 
-# Inntekt: 150-300 
-# Sivilstatus: Ikke gift
-# Kjønn: Kvinne
-# Innvandring: Ikke innvandrer
-# Investering utdanning: Samme som nå
+# Utdanning: Høyere (3)
+# Inntekt: 500-600 (5)
+# Sivilstatus: Gift (4)
+# Kjønn: Kvinne (2)
+# Innvandring: Ikke innvandrer (1)
+# Statsborgerskap: Norsk statsborgerskap (1)
+# Fødselsår: 30-40 (6)
+# Jobb tilstand: Lønnet fulltid (1)
+# Vurdering økonomi: God (2)
+# Interesse politikk: Interresert (2)
+# Vurdering demokrati: Tilfreds (2)
+# Vurdering regjering: Tilfreds (2)
+# Vurdering livet: Tilfreds (2)
+# Trygghet nærområde siste årene: Ingen endring (3)
+# Mening innvandrere bosetter seg: Verken fordel eller ulempe (4)
+# Mening innvandrere: Verken misliker eller liker (4) 
+# Investering utdanning: Bare NA, tas ikke med
 
-
-summary(valgt_df)
-table(valgt_df$fødselsår)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # Lager dummy variabler, bruker kodebok for å se svaralternativer
 
+
+# Dummy for utdanningsnivå 
+#mulig at det er bedre å bruke den andre utdanningsvariabelen
+
+valgt_df <- valgt_df %>%
+  mutate(utdanning = factor(høyest_utdanning, 
+                            levels = c(1, 2, 3),
+                            labels = c("Grunnskole", "Videregående", "Høyere"),
+                            exclude = 97)) %>%
+  mutate(utdanning = fct_relevel(utdanning, "Høyere")) # sammenligningsnivå
+
+
+# Dummy for inntektsnivå 
+
+valgt_df <- valgt_df %>% 
+  mutate(inntekt = factor(brutto_inntekt,
+                          levels = c(1,2,3,4,5,6,7,8),
+                          labels = c("150_og_under", "150-300", "300-400", "400-500", 
+                                     "500-600", "600-700", "700-1000", "1000_og_mer"),
+                          exclude = c(97,98))) %>% 
+  mutate(inntekt = fct_relevel(inntekt, "500-600")) # sammenligningsnivå
+
+
 # Dummy for kjønn 
 valgt_df$mann <- ifelse(valgt_df$kjønn == 1, 1, 0)
 
-# Dummy for alder 
+
+# Dummy for sivilstatus
+# blir gift eller ikke der de andre alternativene blir satt til 0 
+
+valgt_df <- valgt_df %>%
+  mutate(gift_samboer = case_when(
+    sivilstatus %in% c(97, 98) ~ NA_real_,
+    sivilstatus %in% c(1, 2, 5, 6, 7) ~ 0,
+    TRUE ~ 1
+  )) 
+
+
+# Dummy for innvandrer
+
+
+# Dummy statsborgerskap
+
+valgt_df$borgerskap <- ifelse(valgt_df$statsborgerskap == 2, 2, 0)
+
+
+# Dummy for fødselsår 
 valgt_df <- valgt_df %>%
   mutate(aldersgruppe = case_when(
     fødselsår == 1 ~ '1939_og_før',
@@ -139,47 +189,111 @@ valgt_df <- valgt_df %>%
   mutate(aldersgruppe = factor(aldersgruppe, levels = 
     c('1939_og_før', '1940-1949', '1950-1959', '1960-1969', '1970-1979', '1980-1989', '1990_og_etter')))
 
-# Dummy for sivilstatus
 
-# blir gift eller ikke der de andre alternativene blir satt til 0
-# Ser at variabelen har 97 og 98 som må bli fjernet 
-
-valgt_df <- valgt_df %>%
-  mutate(gift_samboer = case_when(
-    sivilstatus %in% c(97, 98) ~ NA_real_,
-    sivilstatus %in% c(1, 2, 5, 6, 7) ~ 0,
-    TRUE ~ 1
-  )) 
-
-# Dummy for utdanningsnivå, #mulig at det er bedre å bruke den andre utdanningsvariabelen
-
-valgt_df <- valgt_df %>%
-  mutate(utdanning = factor(høyest_utdanning, 
-                            levels = c(1, 2, 3),
-                            labels = c("Grunnskole", "Videregående", "Høyere"),
-                            exclude = 97)) %>%
-  mutate(utdanning = fct_relevel(utdanning, "høyere")) # sammenligningsnivå
-
-# Dummy for investering i utdanning
+# Dummy for jobb tilstand
 
 valgt_df <- valgt_df %>% 
-  mutate(investering_utdanningD = factor(investering_utdanning,
-                            levels = c(1, 2, 3, 4, 5, 6),
-                            labels = c("Mye mindre", "Litt mindre", "Det samme", 
-                                       "Litt mer", "Mye mer", "Vet ikke"),
+  mutate(arbeids_tilstand = factor(jobb_tilstand,
+                                         levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+                                         labels = c("Lønnet fulltid", "Lønnet deltid 30", "Lønnet deltid u30", 
+                                                    "Selvdrivende", "Student", "Arbeidsledig søkende",
+                                                    "Arbedisledig ikke-søkende", "Ufør", "Pensjonert",
+                                                    "Militæret", "Hjemmeværende", "Annet"),
+                                         exclude = c(97, 98))) %>% 
+  mutate(arbeids_tilstand = fct_relevel(arbeids_tilstand, "Lønnet fulltid")) # sammenligningsnivå
+
+
+# Dummy for vurdering egen økonomi
+
+valgt_df <- valgt_df %>% 
+  mutate(egenøkonomi = factor(vurdering_egenøkonomi,
+                                   levels = c(1, 2, 3, 4, 5, 6, 7),
+                                   labels = c("Svært god", "God", "Noe god", "Verken god eller dårlig", 
+                                              "Noe dårlig", "Dårlig", "Svært dårlig"),
+                                   exclude = c(97, 98))) %>% 
+  mutate(egenøkonomi = fct_relevel(egenøkonomi, "God")) # sammenligningsnivå
+
+
+# Dummy for interesse politikk 
+
+valgt_df <- valgt_df %>% 
+  mutate(politikk = factor(interesse_politikk,
+                              levels = c(1, 2, 3, 4, 5),
+                              labels = c("Svært interessert", "Interessert", "Noe interresert", 
+                                         "Lite interessert", "Ikke interessert"),
+                              exclude = c(97, 98))) %>% 
+  mutate(politikk = fct_relevel(politikk, "Interessert")) # sammenligningsnivå
+
+
+# Dummy for vurdering av det norske demokratiet
+
+valgt_df <- valgt_df %>% 
+  mutate(demokrati = factor(vurdering_demokrati,
+                           levels = c(1, 2, 3, 4, 5),
+                           labels = c("Svært tilfreds", "Tilfreds", "Noe tilfreds", 
+                                      "Lite tilfreds", "Ikke tilfreds"),
+                           exclude = c(97, 98))) %>% 
+  mutate(demokrati = fct_relevel(demokrati, "Tilfreds")) # sammenligningsnivå
+
+
+# Dummy for vurdering av den norske regjering
+
+valgt_df <- valgt_df %>% 
+  mutate(regjering = factor(vurdering_regjering,
+                            levels = c(1, 2, 3, 4, 5),
+                            labels = c("Svært tilfreds", "Tilfreds", "Noe tilfreds", 
+                                       "Lite tilfreds", "Ikke tilfreds"),
                             exclude = c(97, 98))) %>% 
-  mutate(investering_utdanningD = fct_relevel(investering_utdanningD, "Det samme")) # sammenligningsnivå
+  mutate(regjering = fct_relevel(regjering, "Tilfreds")) # sammenligningsnivå
 
-# Dummy for inntektsnivå 
+
+# Dummy for vurdering av livet
 
 valgt_df <- valgt_df %>% 
-  mutate(inntekt = factor(brutto_inntekt,
-                          levels = c(1,2,3,4,5,6,7,8),
-                          labels = c("150_og_under", "150-300", "300-400", "400-500", 
-                                     "500-600", "600-700", "700-1000", "1000_og_mer"),
-                          exclude = c(97,98))) %>% 
-  mutate(inntekt = fct_relevel(inntekt, "300-400")) # sammenligningsnivå
+  mutate(livet = factor(vurdering_livet,
+                            levels = c(1, 2, 3, 4, 5),
+                            labels = c("Svært tilfreds", "Tilfreds", "Noe tilfreds", 
+                                       "Lite tilfreds", "Ikke tilfreds"),
+                            exclude = c(97, 98))) %>% 
+  mutate(livet = fct_relevel(livet, "Tilfreds")) # sammenligningsnivå
 
+
+# Dummy for trygghet i nærområdet de siste årene
+
+valgt_df <- valgt_df %>% 
+  mutate(trygghet = factor(trygghet_nær,
+                        levels = c(1, 2, 3, 4, 5),
+                        labels = c("Mye tryggere", "Noe tryggere", "Ingen endring", 
+                                   "Noe mer utrygt", "Mye mer utrygt"),
+                        exclude = c(97, 98))) %>% 
+  mutate(trygghet = fct_relevel(trygghet, "Ingen endring")) # sammenligningsnivå
+
+
+
+# Dummy for mening om at innvandrere bosetter seg
+
+valgt_df <- valgt_df %>% 
+  mutate(innvandrere_bosetting = factor(mening_innvandrere_bo,
+                           levels = c(1, 2, 3, 4, 5, 6, 7),
+                           labels = c("Svært stor fordel", "Stor fordel", "En viss fordel", 
+                                      "Verken en fordel eller ulempe", "En viss ulempe",
+                                      "Stor ulempe", "Svært stor ulempe"),
+                           exclude = c(97, 98))) %>% 
+  mutate(innvandrere_bosetting = fct_relevel(innvandrere_bosetting, "Verken en fordel eller ulempe")) # sammenligningsnivå
+
+
+# Dummy for mening om innvandrere 
+
+valgt_df <- valgt_df %>% 
+  mutate(innvandrere = factor(mening_innvandrere,
+                                        levels = c(1, 2, 3, 4, 5, 6, 7),
+                                        labels = c("Svært stor fordel", "Stor fordel", "En viss fordel", 
+                                                   "Verken en fordel eller ulempe", "En viss ulempe",
+                                                   "Stor ulempe", "Svært stor ulempe"),
+                                        exclude = c(97, 98))) %>% 
+  mutate(innvandrere = fct_relevel(innvandrere, "Verken en fordel eller ulempe")) # sammenligningsnivå
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # Henter data for innvandring fra andre runder siden innvandringsstatusen endrer seg ikke over tid
@@ -224,34 +338,16 @@ innvandring_data <- innvandring_data %>%
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# Deskriptiv statistikk
-
-# (Må fikse innvandring, ikke tatt med her)
-
-rdf <- valgt_df %>% 
-  dplyr::select(tillit_stortinget, mann, aldersgruppe, gift_samboer, utdanning, 
-                investering_utdanningD, inntekt)
-
-saveRDS(rdf, "rdf.rds")
-
-rdf <- readRDS("rdf.rds")
-
-# Beregner gjennomsnitt og standardavvik til tillit_stortinget
-tillit_gj <- mean(rdf$tillit_stortinget, na.rm = TRUE)
-tillit_sd <- sd(rdf$tillit_stortinget, na.rm = TRUE)
-
-
-cat("tillit - gjennomsnitt:", tillit_gj, "\n")
-cat("tillit standard avvik:", tillit_sd, "\n")
-
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 # Regresjonsanalyse
 
-rsa <- lm(tillit_stortinget ~ aldersgruppe + utdanning + mann + utdanning + 
-            investering_utdanningD + inntekt + gift_samboer, data = rdf)
+# Innvandring ikke tatt med, må fikses
+
+rsa <- lm(tillit_stortinget ~ utdanning + inntekt + gift_samboer + mann + 
+            borgerskap + aldersgruppe + arbeids_tilstand + egenøkonomi +
+            politikk + demokrati + regjering + livet + trygghet +
+            innvandrere_bosetting + innvandrere, data = rdf)
 
 summary(rsa)
 
 plot(rsa, main = "Model fit")
+
